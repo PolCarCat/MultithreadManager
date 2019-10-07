74c245ec45a646d807a7208bcd04748ff99ae301
albertmas <almaco_io@hotmail.es>
2019-10-08 01:31:05 +0200

    UDP sockets started

--- Sockets/Sockets UDP/Client/main_client.cpp
+++ Sockets/Sockets UDP/Client/main_client.cpp
@@ -30,22 +30,43 @@ void printWSErrorAndExit(const char *msg)
 void client(const char *serverAddrStr, int port)
 {
 	// TODO-1: Winsock init
-
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+		return;
 	// TODO-2: Create socket (IPv4, datagrams, UDP)
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s == INVALID_SOCKET)
+		return;
 
 	// TODO-3: Create an address object with the server address
+	struct sockaddr_in remoteAddr;
+	remoteAddr.sin_family = AF_INET; // IPv4
+	remoteAddr.sin_port = htons(port); // Port
+	const char *remoteAddrStr = "127.0.0.1"; // Not so remote… :-P
+	inet_pton(AF_INET, remoteAddrStr, &remoteAddr.sin_addr);
 
-	while (true)
+	for (int i = 5; i > 0; --i)
 	{
 		// TODO-4:
 		// - Send a 'ping' packet to the server
+		const char* ping = "ping";
+		int msg_out = sendto(s, ping, sizeof(ping), 0, (sockaddr*)&remoteAddr, sizeof(remoteAddr));
+
 		// - Receive 'pong' packet from the server
+		char* pong = "";
+		struct sockaddr_in remoteAddrServer;
+		int sizeRemoteServer = sizeof(remoteAddrServer);
+		int msg_in = recvfrom(s, pong, sizeof(pong), 0, (sockaddr*)&remoteAddrServer, &sizeRemoteServer);
+		std::cout << pong << std::endl;
 		// - Control errors in both cases
 	}
 
 	// TODO-5: Close socket
+	int closesocket(SOCKET s);
 
 	// TODO-6: Winsock shutdown
+	iResult = WSACleanup();
 }
 
 int main(int argc, char **argv)
--- Sockets/Sockets UDP/Server/main_server.cpp
+++ Sockets/Sockets UDP/Server/main_server.cpp
@@ -28,24 +28,55 @@ void printWSErrorAndExit(const char *msg)
 void server(int port)
 {
 	// TODO-1: Winsock init
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+		return;
 
 	// TODO-2: Create socket (IPv4, datagrams, UDP
+	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s == INVALID_SOCKET)
+		return;
 
 	// TODO-3: Force address reuse
+	int enable = 1;
+	int res = setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&enable, sizeof(int));
+	//if (res == SOCKET_ERROR) { // Log and handle error }
 
 	// TODO-4: Bind to a local address
+	struct sockaddr_in bindAddr;
+	bindAddr.sin_family = AF_INET; // IPv4
+	bindAddr.sin_port = htons(port); // Port
+	bindAddr.sin_addr.S_un.S_addr = INADDR_ANY; // Any local IP address
+
+	res = bind(s, (const struct sockaddr *)&bindAddr, sizeof(bindAddr));
+	//if (res == SOCKET_ERROR) { // Log and handle error }
 
 	while (true)
 	{
 		// TODO-5:
 		// - Receive 'ping' packet from a remote host
+		char* ping = "";
+		struct sockaddr_in remoteAddrClient;
+		int sizeRemoteServer = sizeof(remoteAddrClient);
+		int msg_in = recvfrom(s, ping, sizeof(ping), 0, (sockaddr*)&remoteAddrClient, &sizeRemoteServer);
+
 		// - Answer with a 'pong' packet
+		if (ping != "")
+		{
+			std::cout << ping << std::endl;
+			const char* pong = "pong";
+			int msg_out = sendto(s, pong, sizeof(pong), 0, (sockaddr*)&bindAddr, sizeof(bindAddr));
+		}
+
 		// - Control errors in both cases
 	}
 
 	// TODO-6: Close socket
+	int closesocket(SOCKET s);
 
 	// TODO-7: Winsock shutdown
+	iResult = WSACleanup();
 }
 
 int main(int argc, char **argv)
