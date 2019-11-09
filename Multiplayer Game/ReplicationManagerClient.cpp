6513b77322eeccebd338d246bc3ca62cc51cc972
Pol <polcarcat@gmail.com>
2019-11-09 23:52:15 +0100

    Replication mostly done but not tested

--- Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer Game/ReplicationManagerClient.cpp
@@ -1,2 +1,41 @@
 #include "Networks.h"
+#include "ReplicationManagerClient.h"
 
+void ReplicationManagerClient::Read(const InputMemoryStream & packet)
+{
+
+	while (packet.GetSize() > 0) 
+	{
+		ReplicationAction action;
+		uint32 id;
+
+		packet >> action;
+		packet >> id;
+
+		switch (action)
+		{
+		case ReplicationAction::Create:
+			GameObject* newGO = Instantiate();
+			App->modLinkingContext->registerNetworkGameObject(newGO);
+
+			packet >> newGO->position;
+			packet >> newGO->tag;
+
+			break;
+		case ReplicationAction::Update:
+
+			GameObject* go = App->modLinkingContext->getNetworkGameObject(id);
+
+			packet >> go->position;
+			packet >> go->tag;
+			
+			break;
+		case ReplicationAction::Destroy:
+			GameObject* go = App->modLinkingContext->getNetworkGameObject(id);
+			App->modLinkingContext->unregisterNetworkGameObject(go);
+			Destroy(go);
+			
+			break;
+		}
+	}
+}
--- Multiplayer Game/ReplicationManagerServer.h
+++ Multiplayer Game/ReplicationManagerServer.h
@@ -1,2 +1,30 @@
 #pragma once
 
+enum class ReplicationAction
+{
+	None,
+	Create,
+	Update,
+	Destroy
+};
+
+struct ReplicationCommand 
+{
+	ReplicationAction action;
+	uint32 networkId;
+};
+
+class ReplicationManagerServer
+{
+public:
+	void Create(uint32 networkId);
+	void Update(uint32 networkId);
+	void Destroy(uint32 networkId);
+
+	void Write(OutputMemoryStream &packet);
+
+private:
+	std::vector<ReplicationCommand> commands;
+
+};
+
