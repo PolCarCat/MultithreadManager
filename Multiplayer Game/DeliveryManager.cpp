24dd3481ce4ac821b7b8e9d64173f66e9117a6b3
PolCarCat <polcarcat@gmail.com>
2019-11-20 14:49:07 +0100

    start delivery manager

--- Multiplayer Game/DeliveryManager.cpp
+++ Multiplayer Game/DeliveryManager.cpp
@@ -1,2 +1,42 @@
 #include "Networks.h"
+#include "DeliveryManager.h"
 
+Delivery * DeliveryManager::WriteSequenceNumber(OutputMemoryStream & packet)
+{
+
+	packet << nextSeqNumber;
+
+	return nullptr;
+}
+
+bool DeliveryManager::ProccesSequenceNumber(const InputMemoryStream & packet)
+{
+	packet >> nextExpNumber;
+
+	return false;
+}
+
+bool DeliveryManager::HasSequenceNumbersPendingAck() const
+{
+	return !pendingAckNumbers.empty();
+}
+
+void DeliveryManager::WriteSequenceNumbersPendingAck(OutputMemoryStream & packet)
+{
+
+}
+
+void DeliveryManager::ProcessAckdSequenceNumbers(const InputMemoryStream & packet)
+{
+
+}
+
+void DeliveryManager::ProcessTimeOutPackets()
+{
+
+}
+
+void DeliveryManager::Clear()
+{
+
+}
--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,2 +1,57 @@
 #pragma once
 
+
+class DeliveryManager;
+
+class DeliveryDelegate
+{
+public:
+	virtual void OnDeliverySuccess(DeliveryManager* deliveryManager) = 0;
+	virtual void OnDeliveryFailure(DeliveryManager* deliveryManager) = 0;
+
+};
+
+class Delivery
+{
+	uint32 sequenceNumber = 0;
+	double dispatchTime = 0;
+	DeliveryDelegate* delegate = nullptr;
+};
+
+class DeliveryManager 
+{
+public:
+
+	//For senders to write a new seq. numbers into a packet
+	Delivery* WriteSequenceNumber(OutputMemoryStream& packet);
+
+	//For recievers to process the seq. number from an incoming packet
+	bool ProccesSequenceNumber(const InputMemoryStream& packet);
+
+	//For recievers to write ack'ed seq. numbers into a packet
+	bool HasSequenceNumbersPendingAck() const;
+	void WriteSequenceNumbersPendingAck(OutputMemoryStream& packet);
+
+	//For sensers to process ack'ed seq. numbers from packet
+	void ProcessAckdSequenceNumbers(const InputMemoryStream& packet);
+	void ProcessTimeOutPackets();
+
+	void Clear();
+
+private:
+
+	//Private members sender side 
+	// -The next outgoing sequence number
+	// -A list of pending deliveries
+
+	uint32 nextSeqNumber = 0;
+	std::vector<Delivery> pendingDeliveries;
+
+	//Private members reciever side
+	// -Next expeced sequence number
+	// -A list of sequence numbers pending to ack
+	uint32 nextExpNumber = 0;
+	std::vector<uint32> pendingAckNumbers;
+
+
+};
